7e35509f18fb6f7e5ffece5f1fc0157ef658e6e6
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2024-04-04 14:22:40 +0900

    show map

--- gpxquery/ContentView.swift
+++ gpxquery/ContentView.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
+
+struct Location: Identifiable {
+    let id = UUID()
+    let coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
+    let cityHallLocation = Location(coordinate:CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194))
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
     var body: some View {
+        /*
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
@@ -16,6 +28,14 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+         */
+        Map(
+            coordinateRegion: $region,
+            annotationItems: [cityHallLocation]
+        ) {
+            location in MapMarker(coordinate: location.coordinate)
+            
+        }
     }
 }
 
