a652c8228c25e313312b6bcf1a7d50fab648b99a
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2024-04-04 13:28:24 +0900

    Initial Commit

---
+++ gpxquery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gpxquery
+//
+//  Created by ryought on 2024/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gpxquery/gpxqueryApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpxqueryApp.swift
+//  gpxquery
+//
+//  Created by ryought on 2024/04/04.
+//
+
+import SwiftUI
+
+@main
+struct gpxqueryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
